13dd0705dddfb8636349859193ccc49f62dab665
whynotme1131999 <whynotme1131999@gmail.com>
2023-02-26 07:58:42 +0700

    update nhung face book

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
+// import facebookchat from '@/plugins/facebookchat' 
 import '@/style/styles.scss'
 import 'vueperslides/dist/vueperslides.css'
 import router from './router'
+import VueFbCustomerChat from 'vue-fb-customer-chat'
+
+Vue.use(VueFbCustomerChat, {
+  page_id: '104138475948247', //  change 'null' to your Facebook Page ID,
+  theme_color: '#333333', // theme color in HEX
+  locale: 'en_US', // default 'en_US'
+})
 
 Vue.config.productionTip = false
 
