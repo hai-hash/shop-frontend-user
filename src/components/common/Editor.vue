aa9a3aad9dafe55dac6466650371239f80f6d914
hoanga4bg <chuadangki678@gmail.com>
2023-03-22 16:12:57 +0700

    update code

--- src/components/common/Editor.vue
+++ src/components/common/Editor.vue
@@ -1,20 +1,89 @@
 <template>
     <div>
-        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="onReady"
+            @input="$emit('passData', $event)"></ckeditor>
+        <p v-bind="$props">Xem truoc</p>
+        <div id="preview-content" v-html="editorData" style="border: 1px solid black">
+        </div>
     </div>
 </template>
 <script>
-    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-    export default {
-        name: 'EditorWrite',
-        data() {
-            return {
-                editor: ClassicEditor,
-                editorData: '<p>Content of the editor.</p>',
-                editorConfig: {
-                    // The configuration of the editor.
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+export default {
+    name: 'EditorWrite',
+    props: ['editorId'],
+    data() {
+        return {
+            editor: DecoupledEditor,
+            editorData: '',
+            name: '',
+            editorConfig: {
+                toolbar: {
+                    items: [
+                        "selectAll",
+                        "undo",
+                        "redo",
+                        "alignment:left",
+                        "alignment:right",
+                        "alignment:center",
+                        "alignment:justify",
+                        "alignment",
+                        "fontSize",
+                        "fontFamily",
+                        "fontColor",
+                        "fontBackgroundColor",
+                        "bold",
+                        "italic",
+                        "strikethrough",
+                        "underline",
+                        "blockQuote",
+                        "link",
+                        "ckfinder",
+                        "uploadImage",
+                        "imageUpload",
+                        "heading",
+                        "imageTextAlternative",
+                        "toggleImageCaption",
+                        "resizeImage:original",
+                        "resizeImage:25",
+                        "resizeImage:50",
+                        "resizeImage:75",
+                        "resizeImage",
+                        "imageResize",
+                        "imageStyle:inline",
+                        "imageStyle:alignLeft",
+                        "imageStyle:alignRight",
+                        "imageStyle:alignCenter",
+                        "imageStyle:alignBlockLeft",
+                        "imageStyle:alignBlockRight",
+                        "imageStyle:block",
+                        "imageStyle:side",
+                        "imageStyle:wrapText",
+                        "imageStyle:breakText",
+                        "indent",
+                        "outdent",
+                        "numberedList",
+                        "bulletedList",
+                        "mediaEmbed",
+                        // "insertTable",
+                        // "tableColumn",
+                        // "tableRow",
+                        // "mergeTableCells",
+                    ]
                 }
-            };
+            }
+        }
+    },
+    methods: {
+        onReady(editor) {
+            document.getElementById(this.$props['editorId']).prepend(editor.ui.view.toolbar.element);
         }
     }
+}
 </script>
+
+<style scoped>
+#preview-content table {
+    border: 3px solid black !important
+}
+</style>
--- src/view/Blog/Blog.vue
+++ src/view/Blog/Blog.vue
@@ -1,17 +1,46 @@
 <template>
-    <div class="blog">
-        <h1>This is editor, please write anythinh in here</h1>
-        <Editor/>
+    <div>
+        <div id="blog">
+            <lable>Tieu de</lable>
+            <input type="text" v-model="blogData.title" />
+            <lable>Noi dung</lable>
+            <div id="blog-content">
+                <Editor @passData="getContent($event)" v-bind="$props"/>
+            </div>
+            <button @click="showData">Click</button>
+        </div>
     </div>
-
 </template>
 
 <script>
 import Editor from '@/components/common/Editor.vue'
-export default{
-    name:'BlogEditor',
-    components:{
+export default {
+    name: 'BlogEditor',
+    props: {
+        editorId: {
+            type: String,
+            default: 'blog-content',
+        },
+    },
+    components: {
         Editor
+    },
+    methods: {
+        showData: function () {
+            console.log(this.blogData);
+        },
+        getContent(data) {
+            this.blogData.editorData = data;
+        },
+    },
+    data() {
+        return {
+            blogData: {
+                editorData: "",
+                title: ""
+            }
+        }
     }
 }
+
 </script>
